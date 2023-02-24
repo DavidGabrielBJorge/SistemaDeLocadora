6e570c0b28e9d543555a54e380a27c059fb37e21
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-02-24 19:13:22 -0300

    feat: criando a validação do formulario

--- script/main.js
+++ script/main.js
@@ -1,6 +1,30 @@
-function validarform(){
+function adicionar(){
     var tituloFilme = document. getElementById("tituloFilme").value;
     var diretorFilme = document. getElementById("diretorFilme").value;
     var produtoraFilme = document. getElementById("produtoraFilme").value;
     var anoFilme = document. getElementById("anoFilme").value;
+
+    if(tituloFilme == ""){
+        document.getElementById('warning').innerHTML = 'Deve preencher o titulo do filme';
+        return false;
+    }
+    else if(diretorFilme == ""){
+        document.getElementById('warning').innerHTML = 'Deve preencher o diretor do filme';
+        //alert("Deve preencher o diretor do filme")
+        return false;
+    }
+    else if(produtoraFilme == ""){
+        document.getElementById('warning').innerHTML = 'Deve preencher o produtor do filme';
+        //alert("Deve preencher o produtor do filme")
+        return false;
+    }
+    else if(anoFilme == ""){
+        document.getElementById('warning').innerHTML = 'Deve preencher o ano do filme';
+        //alert("Deve preencher o ano do filme")
+        return false;
+    }
+    else{
+        document.getElementById('warning').setAttribute("class", "hidden");
+    }
+
 }

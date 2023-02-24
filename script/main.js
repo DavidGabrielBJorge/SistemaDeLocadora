efaef8557cb1b497e4032a24da77c69911b10e2b
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-02-24 20:11:34 -0300

    feat: criando o adicionar

--- script/main.js
+++ script/main.js
@@ -1,8 +1,8 @@
-function adicionar(){
-    var tituloFilme = document. getElementById("tituloFilme").value;
-    var diretorFilme = document. getElementById("diretorFilme").value;
-    var produtoraFilme = document. getElementById("produtoraFilme").value;
-    var anoFilme = document. getElementById("anoFilme").value;
+function validar(){
+    var tituloFilme = document.getElementById("tituloFilme").value;
+    var diretorFilme = document.getElementById("diretorFilme").value;
+    var produtoraFilme = document.getElementById("produtoraFilme").value;
+    var anoFilme = document.getElementById("anoFilme").value;
 
     if(tituloFilme == ""){
         document.getElementById('warning').innerHTML = 'Deve preencher o titulo do filme';
@@ -27,4 +27,67 @@ function adicionar(){
         document.getElementById('warning').setAttribute("class", "hidden");
     }
 
+    return true;
+}
+
+function mostrarInformacoes(){
+    var filmesLista;
+    if(localStorage.getItem("filmesLista")==null){
+        filmesLista = []
+    }
+    else{
+        filmesLista = JSON.parse(localStorage.getItem("filmesLista"))
+    }
+
+    var espacamento = "";
+
+    filmesLista.forEach(function(element, index){
+        espacamento += "<tr>";
+        espacamento += "<td>" + element.tituloFilme + "</td>";
+        espacamento += "<td>" + element.diretorFilme + "</td>";
+        espacamento += "<td>" + element.produtoraFilme + "</td>";
+        espacamento += "<td>" + element.anoFilme + "</td>";
+        espacamento += 
+        '<td><button onclick="deletar('
+        +index+
+        ')" class="btn btn-danger">Delete</button><button onclick="atualizar('
+        +index+
+        ')" class="btn btn-warning m-2">Atualizar</button></td>'
+        espacamento += "</tr>";
+    });
+    document.querySelector("#crud tbody").innerHTML=espacamento;
+}
+//Ao carregar a página vai mostrar a tabela
+document.onload = mostrarInformacoes();
+
+function adicionar(){
+    if(validar() == true){
+        var tituloFilme = document.getElementById("tituloFilme").value;
+        var diretorFilme = document.getElementById("diretorFilme").value;
+        var produtoraFilme = document.getElementById("produtoraFilme").value;
+        var anoFilme = document.getElementById("anoFilme").value;
+
+        var filmesLista;
+        if(localStorage.getItem("filmesLista")==null){
+            filmesLista = []
+        }
+        else{
+            filmesLista = JSON.parse(localStorage.getItem("filmesLista"))
+        }     
+        filmesLista.push({
+            tituloFilme : tituloFilme,
+            diretorFilme : diretorFilme,
+            produtoraFilme : produtoraFilme,
+            anoFilme : anoFilme
+        })
+
+        localStorage.setItem("filmesLista", JSON.stringify(filmesLista));
+
+        mostrarInformacoes();
+
+        document.getElementById("tituloFilme").value = "";
+        document.getElementById("diretorFilme").value = "";
+        document.getElementById("produtoraFilme").value = "";
+        document.getElementById("anoFilme").value = "";
+    }
 }

ff72e771e15990924a70c42aab3e164d3aad8d27
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-02-24 18:51:27 -0300

    feat: criando a estrutura basica da view, estilizando e iniciando a logica

---
+++ script/main.js
@@ -0,0 +1,6 @@
+function validarform(){
+    var tituloFilme = document. getElementById("tituloFilme").value;
+    var diretorFilme = document. getElementById("diretorFilme").value;
+    var produtoraFilme = document. getElementById("produtoraFilme").value;
+    var anoFilme = document. getElementById("anoFilme").value;
+}

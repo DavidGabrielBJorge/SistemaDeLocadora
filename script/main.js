9f6a59fda5d4a577964c57e4ec5b9ff38cba2610
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-02-27 15:12:45 -0300

    add: criando a funcao de atualizar

--- script/main.js
+++ script/main.js
@@ -39,23 +39,18 @@ function mostrarInformacoes(){
         filmesLista = JSON.parse(localStorage.getItem("filmesLista"))
     }
 
-    var espacamento = "";
+    var elementoHTML = "";
 
     filmesLista.forEach(function(element, index){
-        espacamento += "<tr>";
-        espacamento += "<td>" + element.tituloFilme + "</td>";
-        espacamento += "<td>" + element.diretorFilme + "</td>";
-        espacamento += "<td>" + element.produtoraFilme + "</td>";
-        espacamento += "<td>" + element.anoFilme + "</td>";
-        espacamento += 
-        '<td><button onclick="deletar('
-        +index+
-        ')" class="btn btn-danger">Deletar</button><button onclick="atualizar('
-        +index+
-        ')" class="btn btn-warning m-2">Atualizar</button></td>'
-        espacamento += "</tr>";
+        elementoHTML += "<tr>";
+        elementoHTML += "<td>" + element.tituloFilme + "</td>";
+        elementoHTML += "<td>" + element.diretorFilme + "</td>";
+        elementoHTML += "<td>" + element.produtoraFilme + "</td>";
+        elementoHTML += "<td>" + element.anoFilme + "</td>";
+        elementoHTML += '<td><button onclick="deletar('+index+')" class="btn btn-danger">Deletar</button> <button onclick="atualizar('+index+')" class="btn btn-warning m-2">Atualizar</button></td>'
+        elementoHTML += "</tr>";
     });
-    document.querySelector("#crud tbody").innerHTML=espacamento;
+    document.querySelector("#crud tbody").innerHTML=elementoHTML;
 }
 //Ao carregar a página vai mostrar a tabela
 document.onload = mostrarInformacoes();
@@ -103,4 +98,45 @@ function deletar(index){
     filmesLista.splice(index,1);
     localStorage.setItem("filmesLista",JSON.stringify(filmesLista));
     mostrarInformacoes();
+}
+
+function atualizar(index){
+    //Dessa forma o botão de adicionar some e o de atualizar vai aparecer
+    document.getElementById("submit").style.display="none";
+    document.getElementById("update").style.display="block";
+
+    var filmesLista;
+    if(localStorage.getItem("filmesLista")==null){
+        filmesLista = [];
+    }else{
+        filmesLista = JSON.parse(localStorage.getItem("filmesLista"));
+    }  
+    
+    document.getElementById("tituloFilme").value = filmesLista[index].tituloFilme;
+    document.getElementById("diretorFilme").value = filmesLista[index].diretorFilme;
+    document.getElementById("produtoraFilme").value = filmesLista[index].produtoraFilme;
+    document.getElementById("anoFilme").value = filmesLista[index].anoFilme;
+
+    document.querySelector("#update").onclick = function(){
+        if (validar() == true){
+            filmesLista[index].tituloFilme = document.getElementById("tituloFilme").value;
+            filmesLista[index].diretorFilme = document.getElementById("diretorFilme").value;
+            filmesLista[index].produtoraFilme = document.getElementById("produtoraFilme").value;
+            filmesLista[index].anoFilme = document.getElementById("anoFilme").value;
+
+            localStorage.setItem("filmesLista", JSON.stringify(filmesLista));
+
+            mostrarInformacoes();
+
+            document.getElementById("tituloFilme").value = "";
+            document.getElementById("diretorFilme").value = "";
+            document.getElementById("produtoraFilme").value = "";
+            document.getElementById("anoFilme").value = "";
+
+            //Dessa forma o botão de adicionar aparece e o de atualizar vai desaparecer
+            document.getElementById("submit").style.display="block";
+            document.getElementById("update").style.display="none";
+
+        }
+    }
 }
